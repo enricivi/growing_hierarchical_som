0699c0c738a6c920204319b602334c0631a8c439
Federico Teotini <templarblack.fed03@gmail.com>
2018-03-03 18:13:59 +0100

    Few name refactors

--- single_level/digit_test.py
+++ single_level/digit_test.py
@@ -1,4 +1,4 @@
-from self_organizing_map import SOM
+from single_level.self_organizing_map import SOM
 
 import numpy as np
 from sklearn.datasets import load_digits
@@ -12,8 +12,8 @@ def print_symbols_heat_map(_som, _digits):
     plt.imshow(_som.heat_map(digits.data), cmap='bone')
     markers = ['o', 'v', '1', '3', '8', 's', 'p', 'x', 'D', '*']
     colors = ["r", "g", "b", "y", "c", (0, 0.1, 0.8), (1, 0.5, 0), (1, 1, 0.3), "m", (0.4, 0.6, 0)]
-    m_size = 192/_som.map_dim['w']
-    m_width = 32/_som.map_dim['w']
+    m_size = 192 / _som.map_dim['w']
+    m_width = 32 / _som.map_dim['w']
     for _data, _label in zip(_digits.data, _digits.target):
         marker = markers[_label]
         color = colors[_label]
@@ -29,13 +29,13 @@ def print_symbols_heat_map(_som, _digits):
 
 def print_full_map(_som):
     plt.figure()
-    map_width = 8*_som.map_dim['w']
-    map_height = 8*_som.map_dim['h']
+    map_width = 8 * _som.map_dim['w']
+    map_height = 8 * _som.map_dim['h']
     full_map = np.empty(shape=(map_width, map_height), dtype=np.float32)
     for i in range(0, map_width, 8):
         for j in range(0, map_height, 8):
-            neuron = _som.map[i//8, j//8]
-            full_map[i:(i+8), j:(j+8)] = np.reshape(neuron, newshape=(8, 8))
+            neuron = _som.map[i // 8, j // 8]
+            full_map[i:(i + 8), j:(j + 8)] = np.reshape(neuron, newshape=(8, 8))
     plt.imshow(full_map, cmap='bone_r', interpolation='sinc')
 
 
@@ -46,25 +46,26 @@ n_samples, n_features = data.shape
 n_digits = len(np.unique(digits.target))
 labels = digits.target
 
-print( "dataset length: {}".format(n_samples) )
-print( "features per example: {}".format(n_features) )
-print( "number of digits: {}".format(n_digits) )
+print("dataset length: {}".format(n_samples))
+print("features per example: {}".format(n_features))
+print("number of digits: {}".format(n_digits))
 
-m = {'ed': None, '1d': lambda x, y: np.linalg.norm(x-y, ord=1), 'inf': lambda x, y: np.linalg.norm(x-y, ord=np.inf)}
-som = SOM(map_dim=(16, 16, n_features), metrics=m['ed'], learning_rate=0.5, update_mask_dim=(5, 5), sigma=1.0, decay=0.99)
+m = {'ed': None, '1d': lambda x, y: np.linalg.norm(x - y, ord=1), 'inf': lambda x, y: np.linalg.norm(x - y, ord=np.inf)}
+som = SOM(map_dim=(16, 16, n_features), metrics=m['ed'], learning_rate=0.5, update_mask_dim=(5, 5), sigma=1.0,
+          decay=0.99)
 
 plt.figure(num='gaussian kernel (update_mask)')
 plt.imshow(som.update_mask, cmap='bone')
 plt.colorbar()
 
-som.rand_init_from_data(data=data, seed=1)
+som.rand_init_from_data(dataset=data, seed=1)
 
 print_symbols_heat_map(som, digits)
 print_full_map(som)
 
 start = time()
-som.train(data=data, epochs=10, seed=2)
-print( "elpased time: {:.1f} seconds".format(time()-start) )
+som.train(dataset=data, epochs=10, seed=2)
+print("elapsed time: {:.1f} seconds".format(time() - start))
 
 print_symbols_heat_map(som, digits)
 print_full_map(som)
--- single_level/self_organizing_map.py
+++ single_level/self_organizing_map.py
@@ -14,38 +14,38 @@ class SOM:
         :param decay: learning rate decay factor
         """
         self.map = np.empty(shape=map_dim, dtype=np.float32)
-        self.map_dim = { 'w':  map_dim[0], 'h':  map_dim[1], 'depth':  map_dim[2] }
+        self.map_dim = {'w': map_dim[0], 'h': map_dim[1], 'depth': map_dim[2]}
 
         self.learning_rate = learning_rate
         self.decay = decay
 
         self.update_mask = np.zeros(shape=update_mask_dim, dtype=np.float32)
-        self.update_mask[int((update_mask_dim[0]-1)/2), int((update_mask_dim[1]-1)/2)] = 1.0
+        self.update_mask[int((update_mask_dim[0] - 1) / 2), int((update_mask_dim[1] - 1) / 2)] = 1.0
         self.update_mask = gaussian_filter(self.update_mask, sigma=sigma)
 
         self.metrics = metrics
         if metrics is None:
-            self.metrics = lambda x, y: np.linalg.norm(x-y)
+            self.metrics = lambda x, y: np.linalg.norm(x - y)
 
-    def rand_init_from_data(self, data, seed=None):
+    def rand_init_from_data(self, dataset, seed=None):
         """ initializes the map of the SOM picking random samples from data """
         random_generator = np.random.RandomState(seed)
         for idx in np.ndindex(self.map_dim["w"], self.map_dim["h"]):
-            self.map[idx] = data[int(random_generator.rand()*(len(data) - 1))]
+            self.map[idx] = dataset[int(random_generator.rand() * (len(dataset) - 1))]
             self.map[idx] /= np.linalg.norm(self.map[idx])
 
-    def activate(self, X):
+    def activate(self, data_item):
         """
-        :param X: a sample from the data (length = map_dim[2])
+        :param data_item: a sample from the data (length = map_dim[2])
         """
         activations = np.empty(shape=(self.map_dim["w"], self.map_dim["h"]), dtype=np.float32)
         it = np.nditer(activations, flags=['multi_index'])
         while not it.finished:
-            activations[it.multi_index] = self.metrics(X, self.map[it.multi_index[0], it.multi_index[1]])
+            activations[it.multi_index] = self.metrics(data_item, self.map[it.multi_index[0], it.multi_index[1]])
             it.iternext()
         return activations
 
-    def train(self, data, epochs=5, seed=None):
+    def train(self, dataset, epochs=5, seed=None):
         """
         updates the weights of the neurons
         """
@@ -53,27 +53,27 @@ class SOM:
         random_generator = np.random.RandomState(seed)
         lr = self.learning_rate
         for epoch in range(epochs):
-            for idx in random_generator.permutation(len(data)):
-                lr = lr*self.decay
+            for idx in random_generator.permutation(len(dataset)):
+                lr = lr * self.decay
 
-                x = data[idx]
+                x = dataset[idx]
                 winner = self.min_activation_idx(x)
 
-                dx = int((self.update_mask.shape[0]-1)/2)
-                dy = int((self.update_mask.shape[1]-1)/2)
+                dx = int((self.update_mask.shape[0] - 1) / 2)
+                dy = int((self.update_mask.shape[1] - 1) / 2)
                 it = np.nditer(self.update_mask, flags=['multi_index'])
                 while not it.finished:
                     i = winner[0] + (it.multi_index[0] - dx)
                     j = winner[1] + (it.multi_index[1] - dy)
                     if (i >= 0) and (j >= 0) and (i < self.map.shape[0]) and (j < self.map.shape[1]):
-                        self.map[i, j] += lr*self.update_mask[it.multi_index]*(x - self.map[i, j])
+                        self.map[i, j] += lr * self.update_mask[it.multi_index] * (x - self.map[i, j])
                         self.map[i, j] /= np.linalg.norm(self.map[i, j])
                     it.iternext()
-            print( "   --> epoch {} (of {}) finished".format(epoch+1, epochs) )
+            print("   --> epoch {} (of {}) finished".format(epoch + 1, epochs))
         print("end training\n")
 
-    def min_activation_idx(self, data):
-        activations = self.activate(data)
+    def min_activation_idx(self, data_item):
+        activations = self.activate(data_item)
         return np.unravel_index(activations.argmin(), activations.shape)
 
     def heat_map(self, dataset):
