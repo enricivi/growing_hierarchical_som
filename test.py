f0c57dfd71a8b259933d10a54c93954664d11a78
Enrico Civitelli <enrico.civitelli@gmail.com>
2018-03-15 18:55:11 +0100

    added normalize_dataset function
    in test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import numpy as np
 from sklearn.datasets import load_digits
-from collections import OrderedDict
 from time import time
 import matplotlib.pyplot as plt
 from GHSOM import GHSOM
+
+
+def normalize_dataset(dataset):
+    for idx, data in enumerate(dataset):
+        norm_data = data / np.linalg.norm(data)
+        dataset[idx] = norm_data
 
 
 digits = load_digits()
@@ -18,6 +23,9 @@ print("features per example: {}".format(n_features))
 print("number of digits: {}\n".format(n_digits))
 
 ghsom = GHSOM(t1=1, t2=1, learning_rate=0.5, decay=0.99, gaussian_sigma=1, epoch_number=5)
+
+#print("Normalizing dataset...")
+#normalize_dataset(data)
 
 print("Training...")
 start = time()
